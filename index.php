e06e4f532141b7a1e055661b9931040bc4ae3410
DylanSmithSE <dylansmith.se@gmail.com>
2022-08-10 15:09:55 -0700

    added bootstrap for styling

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $conn->close();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test Blog</title>
-    <link rel="stylesheet" href="../boostwatch/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap.min.css">
 </head>
 
 <body>
